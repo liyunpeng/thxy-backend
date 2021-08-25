4f4d1a0ea4e241c9cc835c67f295bb481ec2926f
李刚 <ligang@ipfsforce.com>
2021-08-25 12:00:49 +0800

    course表保存接收的上传文件保存到哪个目录

--- model/course.go
+++ model/course.go
@@ -5,6 +5,7 @@ type Course struct {
 	Title     string `json:"title"`
 	Introduce string `json:"introduce"`
 	Provider  string `json:"provider"`
+	StorePath string `json:"store_path"`
 	TypeId    int    `json:"type_id"`
 	CataLevel int    `json:"cata_level"` // 1：一级目录， 点中之后， 直接进文件， 2：存在二级目录， 点中之后进下一级
 }
